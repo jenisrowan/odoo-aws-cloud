2d6e26da1bb684de96566a8d734b08ec259e69c2
jenisrowan <jenisrowan0606@gmail.com>
2026-03-15 10:45:40 +0530

    feat: configure S3 backend and ECR force delete for testing
    - Added S3 backend configuration to [provider.tf](cci:7://file:///c:/odoo/odoo-aws-cloud/provider.tf:0:0-0:0) with state locking via DynamoDB.
    - Set `force_delete = true` on ECR repositories to simplify cleanup during the testing phase.

--- ecr.tf
+++ ecr.tf
@@ -2,6 +2,7 @@
 resource "aws_ecr_repository" "odoo" {
   name                 = "odoo-custom"
   image_tag_mutability = "MUTABLE"
+  force_delete = true # Only for testing
 
   image_scanning_configuration {
     scan_on_push = true
@@ -12,6 +13,7 @@ resource "aws_ecr_repository" "odoo" {
 resource "aws_ecr_repository" "nginx" {
   name                 = "nginx-custom"
   image_tag_mutability = "MUTABLE"
+  force_delete = true # Only for testing
 
   image_scanning_configuration {
     scan_on_push = true
--- provider.tf
+++ provider.tf
@@ -5,6 +5,13 @@ terraform {
       version = "~>5.0"
     }
   }
+  backend "s3" {
+    bucket         = "odoo-aws-cloud-s3"
+    key            = "odoo-prod/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "odoo-terraform-state-locks"
+    encrypt        = true
+  }
 }
 
 provider "aws" {
