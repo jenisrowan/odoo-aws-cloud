cf7b8810a7db3a1a3db51b921cd039b49fd415d4
jenisrowan <jenisrowan0606@gmail.com>
2026-03-15 15:43:59 +0530

    feat(efs): add 3-tier intelligent tiering with archive storage
    - Added IA transition after 60 days of no access
    - Added Archive transition after 180 days (6 months) of no access
    - Archive storage is available because EFS is configured as regional
    - Files are automatically promoted back to Standard on first access

--- efs.tf
+++ efs.tf
@@ -1,8 +1,23 @@
 resource "aws_efs_file_system" "odoo" {
   creation_token   = "odoo-efs"
   performance_mode = "generalPurpose"
+  throughput_mode  = "bursting"
 
-  throughput_mode = "bursting"
+  # Intelligent Tiering: move to IA after 60 days (~2 months) of no access
+  lifecycle_policy {
+    transition_to_ia = "AFTER_60_DAYS"
+  }
+
+  # Archive storage: move to Archive if not accessed for 6 months
+  # We can use Archive storage class because we are using regional EFS
+  lifecycle_policy {
+    transition_to_archive = "AFTER_180_DAYS"
+  }
+
+  # Move back to Standard storage immediately on first access
+  lifecycle_policy {
+    transition_to_primary_storage_class = "AFTER_1_ACCESS"
+  }
 }
 
 resource "aws_efs_mount_target" "efs_mount" {
